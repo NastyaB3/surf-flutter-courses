6aae20c3b19d82c1839ac49adec7d82ca2b71d25
anastasia <anastasiabelyakova9@gmail.com>
2023-06-18 21:55:06 +0300

    task 04: description of Strategy and print card

---
+++ bin/04_task/04_task_bin.dart
@@ -0,0 +1,27 @@
+typedef Strategy = double Function (List<String> p0, List<String> p1);
+class PokerPlayer {
+  /// Список текущих карт в руке у игрока
+  List<String> _currentHand = ['King of clubs', 'Nine of hearts'];
+
+  /// Субъективная оценка выигрыша
+  double _surenessInWin = 0;
+
+  /// Вычислить шансы на выигрыш
+  void calculateProbabilities(
+      List<String> cardOnDesk,
+      Strategy strategy,) => _surenessInWin = strategy(cardOnDesk, _currentHand);
+}
+
+
+void main() {
+  final opponent = PokerPlayer();
+  final Strategy fakeStrategy = (p0, p1) {
+    print('У противника есть карты: ${p0.join(',')}');
+    return 2.0;
+  };
+
+  opponent.calculateProbabilities(
+    ['Nine of diamonds', 'king of hearts'],
+    fakeStrategy,
+  );
+}
