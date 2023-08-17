b8052e3a6f48b1cf1417ced9e0758460eb02b96f
anastasia <anastasiabelyakova9@gmail.com>
2023-08-17 11:28:19 +0300

    task 10: Card

---
+++ flutterapp/lib/10-task/utils/strings.dart
@@ -0,0 +1,12 @@
+class Strings {
+
+  static var name = 'Анастасия Белякова';
+  static var profession = 'Flutter разработчик';
+  static var intro ='Я люблю путешествовать и изучать новые технологии. Я люблю пробовать разные активности и виды спорта';
+  static var emoji1 = 'assets/images/emoji1.png';
+  static var emoji2 ='assets/images/emoji2.png';
+  static var emoji3 ='assets/images/emoji3.png';
+  static var myImage ='assets/images/my_image.jpg';
+
+
+}
---
+++ flutterapp/lib/10-task/utils/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static TextStyle header = const TextStyle(
+    fontSize: 35,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle subtitle = const TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w300,
+  );
+  static TextStyle text = const TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w400,
+  );
+}
